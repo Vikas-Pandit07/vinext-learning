0fa0bd9061a7cabad72925d38c87568b6bfa26c4
Vikas Pandit <vikaaspanditt369@gmail.com>
2026-02-11 18:21:09 +0530

    chore: intialize Spring Boot project

---
+++ backend/vinext-learning/src/main/java/com/vinext/VinextLearningApplication.java
@@ -0,0 +1,13 @@
+package com.vinext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinextLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinextLearningApplication.class, args);
+	}
+
+}
---
+++ backend/vinext-learning/src/test/java/com/vinext/VinextLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinextLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
