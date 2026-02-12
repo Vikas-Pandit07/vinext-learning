6594c0bdaddf1752860e22500039132634ff3327
Vikas Pandit <vikaspandit663@gmail.com>
2026-02-12 15:45:37 +0530

    refactor(config): migrate configuration from properties to YAML format

--- backend/src/main/java/com/vinext/VinextLearningApplication.java
+++ backend/src/main/java/com/vinext/VinextLearningApplication.java
@@ -1,4 +1,4 @@
-package com.vinextlearning;
+package com.vinext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/vinext/VinextlearningApplicationTests.java
+++ backend/src/test/java/com/vinext/VinextlearningApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.vinext;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VinextlearningBackendApplicationTests {
+class VinextlearningApplicationTests {
 
 	@Test
 	void contextLoads() {
